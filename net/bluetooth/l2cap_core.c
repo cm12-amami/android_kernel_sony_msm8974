0269ba02d7cb56043781b590b5b4b729c7b72346
DerfElot <frederic.koch@gmail.com>
2016-07-10 02:01:39 +0200

    fix conflicts

--- net/bluetooth/l2cap_core.c
+++ net/bluetooth/l2cap_core.c
@@ -5430,15 +5430,7 @@ static void l2cap_logical_link_complete(struct hci_chan *chan, u8 status)
 	struct hci_chan *ampchan;
 	struct hci_conn *ampcon;
 
-<<<<<<< HEAD
 	BT_DBG("status %d, chan %p, conn %p", (int) status, chan, chan->conn);
-=======
-	if (chan == NULL) {
-		BT_ERR("%s: status %d, chan %pK", __func__, (int) status, chan);
-		return;
-	}
-	BT_DBG("status %d, chan %pK, conn %pK", (int) status, chan, chan->conn);
->>>>>>> 2746ed9... Bluetooth: Replace %p with %pK
 
 	sk = chan->l2cap_sk;
 	chan->l2cap_sk = NULL;
