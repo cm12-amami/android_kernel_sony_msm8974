9a727491a14db96e863e18b657cfd99ad26aabe9
DerfElot <frederic.koch@gmail.com>
2016-08-20 23:56:33 +0200

    Elevation of Privilege Vulnerability in Performance Module
    CVE-2016-3768

--- arch/arm/mach-msm/perf_event_msm_l2.c
+++ arch/arm/mach-msm/perf_event_msm_l2.c
@@ -836,8 +836,11 @@ static int msm_l2_test_set_ev_constraint(struct perf_event *event)
 			 * This sets the event OFF on all but one
 			 * CPU.
 			 */
-			if (!(event->cpu < 0))
+			if (!(event->cpu < 0)) {
 				event->state = PERF_EVENT_STATE_OFF;
+ 				event->attr.constraint_duplicate = 1;
+				err = -EPERM;
+			}
 	}
 
 out:
