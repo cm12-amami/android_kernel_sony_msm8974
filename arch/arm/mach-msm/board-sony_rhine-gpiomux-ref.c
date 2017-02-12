ef6127ef951e062c38755f304ca72d0ee3458184
DerfElot <frederic.koch@gmail.com>
2017-02-12 02:38:52 +0100

    Revert "amami: Fix compilation warning in board-sony_rhine-gpiomux-ref.c"
    This reverts commit 76c231a158f2aa8e9a0504a0088262f2460d4e20.

--- arch/arm/mach-msm/board-sony_rhine-gpiomux-ref.c
+++ arch/arm/mach-msm/board-sony_rhine-gpiomux-ref.c
@@ -162,6 +162,29 @@ static struct gpiomux_setting gpio_eth_config = {
 	.func = GPIOMUX_FUNC_GPIO,
 };
 
+static struct gpiomux_setting gpio_spi_cs2_config = {
+	.func = GPIOMUX_FUNC_4,
+	.drv = GPIOMUX_DRV_6MA,
+	.pull = GPIOMUX_PULL_DOWN,
+};
+
+static struct gpiomux_setting gpio_spi_config = {
+	.func = GPIOMUX_FUNC_1,
+	.drv = GPIOMUX_DRV_12MA,
+	.pull = GPIOMUX_PULL_NONE,
+};
+static struct gpiomux_setting gpio_spi_susp_config = {
+	.func = GPIOMUX_FUNC_GPIO,
+	.drv = GPIOMUX_DRV_2MA,
+	.pull = GPIOMUX_PULL_DOWN,
+};
+
+static struct gpiomux_setting gpio_spi_cs1_config = {
+	.func = GPIOMUX_FUNC_GPIO,
+	.drv = GPIOMUX_DRV_6MA,
+	.pull = GPIOMUX_PULL_UP,
+};
+
 static struct msm_gpiomux_config msm_eth_configs[] = {
 	{
 		.gpio = KS8851_IRQ_GPIO,
